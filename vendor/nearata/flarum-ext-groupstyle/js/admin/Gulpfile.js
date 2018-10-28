4c1976e04b475e09046507ca0af587d521c91744
ExtBot <flarums-com@gooof.de>
2018-10-28 04:43:46 +0100

    composer update 28.10.2018

--- vendor/nearata/flarum-ext-groupstyle/js/admin/Gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('flarum-gulp');
-
-gulp({
-  modules: {
-    'nearata/flarum-ext-groupstyle': [
-      'src/**/*.js',
-    ]
-  }
-});
--- vendor/nearata/flarum-ext-groupstyle/js/admin/src/main.js
+++
@@ -1,5 +0,0 @@
-import app from 'flarum/app';
-
-app.initializers.add('nearata/flarum-ext-groupstyle', () => {
-  console.log('Hello, admin!');
-});
