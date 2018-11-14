c072ec97626b416ee5d66d368ed94cbc0d02e9bd
ExtBot <flarums-com@gooof.de>
2018-11-14 03:48:40 +0100

    composer update 14.11.2018

--- vendor/flagrow/impersonate/src/Controllers/LoginController.php
+++ vendor/flagrow/impersonate/src/Controllers/LoginController.php
@@ -2,18 +2,15 @@
 
 namespace Flagrow\Impersonate\Controllers;
 
-use Dflydev\FigCookies\FigResponseCookies;
-use Dflydev\FigCookies\SetCookies;
 use Flarum\Api\Serializer\UserSerializer;
-use Flarum\Http\CookieFactory;
 use Flarum\Http\Rememberer;
 use Flarum\Http\SessionAuthenticator;
 use Flarum\User\AssertPermissionTrait;
 use Flarum\User\User;
+use Illuminate\Contracts\Session\Session;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Zend\Diactoros\Response\JsonResponse;
 
 class LoginController implements RequestHandlerInterface
@@ -22,15 +19,13 @@ class LoginController implements RequestHandlerInterface
 
     protected $authenticator;
     protected $rememberer;
-    protected $cookies;
 
     public $serializer = UserSerializer::class;
 
-    public function __construct(SessionAuthenticator $authenticator, Rememberer $rememberer, CookieFactory $cookies)
+    public function __construct(SessionAuthenticator $authenticator, Rememberer $rememberer)
     {
         $this->authenticator = $authenticator;
         $this->rememberer = $rememberer;
-        $this->cookies = $cookies;
     }
 
     /**
@@ -51,29 +46,11 @@ class LoginController implements RequestHandlerInterface
         $this->assertCan($request->getAttribute('actor'), 'flagrowCanImpersonate', $user);
 
         /**
-         * @var $session SessionInterface
+         * @var $session Session
          */
         $session = $request->getAttribute('session');
         $this->authenticator->logIn($session, $user->id);
 
-        $response = $this->rememberer->forget(new JsonResponse(true));
-
-        $dummyCookie = $this->cookies->make('dummy');
-
-        // Fix the Remember::forget() method not using the cookie factory
-        // And as such having the wrong cookie path
-        // This is already fixed in beta8
-        foreach (SetCookies::fromResponse($response)->getAll() as $cookie) {
-            // Only alter expired cookies
-            if ($cookie->getExpires() < time()) {
-                $response = FigResponseCookies::set($response, $cookie
-                    ->withPath($dummyCookie->getPath())
-                    ->withSecure($dummyCookie->getSecure())
-                    ->withHttpOnly($dummyCookie->getHttpOnly())
-                );
-            }
-        }
-
-        return $response;
+        return $this->rememberer->forget(new JsonResponse(true));
     }
 }
