bcf791f2b61473e84b83739f5d79b57412f76e91
ExtBot <flarums-com@gooof.de>
2018-12-03 03:28:36 +0100

    composer update 03.12.2018

--- vendor/FriendsOfFlarum/spamblock/src/Controllers/MarkAsSpammerController.php
+++ vendor/FriendsOfFlarum/spamblock/src/Controllers/MarkAsSpammerController.php
@@ -1,21 +1,30 @@
 <?php
 
+/*
+ * This file is part of fof/spamblock.
+ *
+ * Copyright (c) 2018 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\Spamblock\Controllers;
 
 use Carbon\Carbon;
+use Flarum\Discussion\Command\EditDiscussion;
 use Flarum\Extension\ExtensionManager;
-use Flarum\User\User;
-use Zend\Diactoros\Response;
 use Flarum\Post\Command\EditPost;
-use Flarum\User\Command\EditUser;
-use Flarum\Post\Command\DeletePost;
 use Flarum\User\AssertPermissionTrait;
-use Psr\Http\Message\ResponseInterface;
+use Flarum\User\Command\EditUser;
+use Flarum\User\User;
+use FoF\Spamblock\Event\MarkedUserAsSpammer;
 use Illuminate\Contracts\Bus\Dispatcher;
+use Illuminate\Contracts\Events\Dispatcher as EventsDispatcher;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Flarum\Discussion\Command\EditDiscussion;
-use Flarum\Discussion\Command\DeleteDiscussion;
+use Zend\Diactoros\Response;
 
 class MarkAsSpammerController implements RequestHandlerInterface
 {
@@ -27,23 +36,32 @@ class MarkAsSpammerController implements RequestHandlerInterface
     protected $bus;
 
     /**
+     * @var EventsDispatcher
+     */
+    protected $events;
+
+    /**
      * @var ExtensionManager
      */
     protected $extensions;
 
     /**
-     * @param Dispatcher $bus
+     * @param EventsDispatcher $events
+     * @param Dispatcher       $bus
      * @param ExtensionManager $extensions
      */
-    public function __construct(Dispatcher $bus, ExtensionManager $extensions)
+    public function __construct(Dispatcher $bus, EventsDispatcher $events, ExtensionManager $extensions)
     {
         $this->bus = $bus;
+        $this->events = $events;
         $this->extensions = $extensions;
     }
 
     /**
      * Handle the request and return a response.
+     *
      * @param ServerRequestInterface $request
+     *
      * @return ResponseInterface
      */
     public function handle(ServerRequestInterface $request) : ResponseInterface
@@ -58,7 +76,7 @@ class MarkAsSpammerController implements RequestHandlerInterface
         if ($this->extensions->isEnabled('flarum-suspend') && !isset($user->suspended_until)) {
             $this->bus->dispatch(
                 new EditUser($user->id, $actor, [
-                    'attributes' => ['suspendedUntil' => Carbon::now()->addYear(20)]
+                    'attributes' => ['suspendedUntil' => Carbon::now()->addYear(20)],
                 ])
             );
         }
@@ -67,7 +85,7 @@ class MarkAsSpammerController implements RequestHandlerInterface
             foreach ($posts as $post) {
                 $this->bus->dispatch(
                     new EditPost($post->id, $actor, [
-                        'attributes' => ['isHidden' => true]
+                        'attributes' => ['isHidden' => true],
                     ])
                 );
             }
@@ -77,11 +95,15 @@ class MarkAsSpammerController implements RequestHandlerInterface
             foreach ($discussions as $discussion) {
                 $this->bus->dispatch(
                     new EditDiscussion($discussion->id, $actor, [
-                        'attributes' => ['isHidden' => true]
+                        'attributes' => ['isHidden' => true],
                     ])
                 );
-            };
+            }
         });
+
+        $this->events->dispatch(
+            new MarkedUserAsSpammer($user, $actor)
+        );
 
         return (new Response())->withStatus(204);
     }
