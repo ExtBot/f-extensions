5026e64d8fb5b52f5619e5425e0df55d11f5aff4
ExtBot <flarums-com@gooof.de>
2019-04-02 03:56:34 +0200

    composer update 02.04.2019

--- vendor/flagrow/ads/js/src/admin/index.js
+++
@@ -1,8 +0,0 @@
-import {extend} from "flarum/extend";
-import app from "flarum/app";
-import addAdsPane from "./addAdsPane";
-
-app.initializers.add('flagrow-ads', () => {
-    // add the admin pane
-    addAdsPane();
-});
