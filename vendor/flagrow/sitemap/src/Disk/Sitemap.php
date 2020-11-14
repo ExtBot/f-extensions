d6c2f6087c69a6de9b46aa612c64c8e55e270b09
ExtBot <flarums-com@gooof.de>
2020-11-14 03:21:56 +0100

    composer update 14.11.2020

--- vendor/flagrow/sitemap/src/Disk/Sitemap.php
+++ vendor/flagrow/sitemap/src/Disk/Sitemap.php
@@ -13,6 +13,7 @@
 namespace FoF\Sitemap\Disk;
 
 use Carbon\Carbon;
+use Flarum\Foundation\Paths;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -50,7 +51,7 @@ class Sitemap
      */
     public function write(): array
     {
-        $directory = $this->tmpDir ?? public_path('sitemaps');
+        $directory = $this->tmpDir ?? app(Paths::class)->public.DIRECTORY_SEPARATOR.'sitemaps';
 
         if (!is_dir($directory)) {
             mkdir($directory, 0777, true);
