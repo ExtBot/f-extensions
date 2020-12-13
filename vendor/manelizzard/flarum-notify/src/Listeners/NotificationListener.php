5b0ff074b33ae2864827d0ee42cc70b2594e6c52
ExtBot <flarums-com@gooof.de>
2020-12-13 03:22:57 +0100

    composer update 13.12.2020

---
+++ vendor/manelizzard/flarum-notify/src/Listeners/NotificationListener.php
@@ -0,0 +1,64 @@
+<?php namespace Moay\Notify\Listeners;
+
+use Flarum\Settings\SettingsRepositoryInterface;
+use Moay\Notify\Connectors\SlackConnector;
+use Moay\Notify\Connectors\HipChatConnector;
+use Moay\Notify\Connectors\GitterConnector;
+use Moay\Notify\Connectors\TelegramConnector;
+
+class NotificationListener
+{
+    protected $settings;
+
+    public function __construct(SettingsRepositoryInterface $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /**
+     * Returns all connectors that need to be notified
+     * @return array  The connectors
+     */
+    protected function getConnectorsToNotify()
+    {
+        $connectors = [];
+
+        // Check for slack
+        if ($this->settings->get('flarum-notify.slackEnabled') &&
+            $this->settings->get('notify.slack.token') &&
+            $this->settings->get('notify.slack.channel') &&
+            SlackConnector::isAvailable()
+        ) {
+            $connectors[] = new SlackConnector($this->settings);
+        }
+
+        // Check for HipChat
+        if ($this->settings->get('flarum-notify.hipChatEnabled') &&
+            $this->settings->get('notify.hipchat.token') &&
+            $this->settings->get('notify.hipchat.room') &&
+            HipChatConnector::isAvailable()
+        ) {
+            $connectors[] = new HipChatConnector($this->settings);
+        }
+
+        // Check for Gitter
+        if ($this->settings->get('flarum-notify.gitterEnabled') &&
+            $this->settings->get('notify.gitter.webhook') &&
+            GitterConnector::isAvailable()
+        ) {
+            $connectors[] = new GitterConnector($this->settings);
+        }
+
+        // Check for Telegram
+        if ($this->settings->get('flarum-notify.telegramEnabled') &&
+            $this->settings->get('notify.telegram.apiKey') &&
+            $this->settings->get('notify.telegram.botName') &&
+            $this->settings->get('notify.telegram.chatId') &&
+            TelegramConnector::isAvailable()
+        ) {
+            $connectors[] = new TelegramConnector($this->settings);
+        }
+
+        return $connectors;
+    }
+}
